3b5aea49a01ed03e12bf025dc4fafb7d044f9286
Mak <mahmoudcode913@gmail.com>
2021-09-04 13:51:45 -0500

    modfied rps.js to have if statements and added afunction to test the rps

--- rps.js
+++ rps.js
@@ -1,14 +1,38 @@
+
+
 function computerPlay() { //computer generates a random answer.
     let getComputerPlay = (Math.floor(Math.random() * 10)) % 3;
     if (getComputerPlay === 0){
-        return "Rock";
+        return "rock";
     }else if (getComputerPlay === 1){
-        return " Paper";
+        return " paper";
     }else if (getComputerPlay === 2){
-        return "Scissors";
+        return "scissors";
     }
     
     
 
 };
 
+function playRound(player,aBot){
+    if (player === "rock" && aBot === "paper"){
+        return " you lose!";
+    }
+    else if (player === "rock" && aBot === "scissors"){
+        return "you win!";
+    }
+    else if (player === "paper" && aBot === "scissors"){
+        return "you lose!";
+    }
+    else if (player === "paper" && aBot === "rock"){
+        return "you win!";
+    }
+    else {
+        return "it is a tie"
+    }
+
+};
+
+const player = "rock";
+const aBot = computerPlay();
+console.log(playRound(player, aBot));
